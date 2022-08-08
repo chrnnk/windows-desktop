9f96a8b0fbd42d60090c96d9417e04c4f5c3d373
chrxnn <nick@chrxnn.com>
2022-08-08 14:09:12 -0400

    Update Install-Apps-Winget.ps1

--- Install-Apps-Winget.ps1
+++ Install-Apps-Winget.ps1
@@ -1,1 +1,13 @@
 winget install -e --id dnGrep.dnGrep
+winget install -e --id 7zip.7zip
+winget install -e --id Discord.Discord
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Plex.Plex
+winget install -e --id Valve.Steam
+winget install -e --id tailscale.tailscale
+winget install -e --id Obsidian.Obsidian
+winget install -e --id Logitech.GHUB
+winget install -e --id dnGrep.dnGrep
+winget install -e --id Microsoft.VisualStudioCode
+winget upgrade --all
