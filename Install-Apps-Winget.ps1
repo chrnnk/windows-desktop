d0253b203359cc156b338eb29eacffdb99d451d7
Nick Nelson <nicholasadamnelson@gmail.com>
2022-08-19 23:47:21 -0700

    Update Install-Apps-Winget.ps1

--- Install-Apps-Winget.ps1
+++ Install-Apps-Winget.ps1
@@ -1,14 +1,39 @@
 winget export -o c:\temp\installed.log
-# winget install -e --id dnGrep.dnGrep
 winget install -e --id 7zip.7zip
 winget install -e --id Discord.Discord
+winget install -e --id dnGrep.dnGrep
+winget install -e --id EpicGames.EpicGamesLauncher
 winget install -e --id GitHub.GitHubDesktop
-winget install -e --id Microsoft.WindowsTerminal
-winget install -e --id Plex.Plex
-winget install -e --id Valve.Steam
-winget install -e --id tailscale.tailscale
-winget install -e --id Obsidian.Obsidian
+winget install -e --id Git.Git
+winget install -e --id JohnMacFarlane.Pandoc
 winget install -e --id Logitech.GHUB
-winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Mozilla.Firefox
+winget install -e --id Nvidia.PhysX
+winget install -e --id Nvidia.Broadcast
+winget install -e --id Obsidian.Obsidian
+winget install -e --id Oracle.JavaRuntimeEnvironment
+winget install -e --id Plex.Plex
+winget install -e --id qBittorrent.qBittorrent
+winget install -e --id tailscale.tailscale
+winget install -e --id Valve.Steam
+winget install -e --id Microsoft.Edge
+winget install -e --id Microsoft.EdgeWebView2Runtime
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Microsoft.Office
+winget install -e --id Microsoft.OneDrive
+winget install -e --id Microsoft.Teams
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.VC++2013Redist-x64
+winget install -e --id Microsoft.VC++2010Redist-x64
+winget install -e --id Microsoft.DotNet.DesktopRuntime.3_1
+winget install -e --id Microsoft.VC++2012Redist-x86
+winget install -e --id Microsoft.VC++2015-2022Redist-x64
+winget install -e --id Microsoft.VC++2008Redist-x64
+winget install -e --id Microsoft.VC++2005Redist-x86
+winget install -e --id Microsoft.VC++2008Redist-x86
+winget install -e --id Microsoft.VC++2013Redist-x86
+winget install -e --id Microsoft.VC++2010Redist-x86
+winget install -e --id Microsoft.VC++2015-2022Redist-x86
+winget install -e --id Microsoft.VC++2005Redist-x64
+winget install -e --id Microsoft.VC++2012Redist-x64
 winget upgrade --all
