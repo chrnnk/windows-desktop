bada05c2e956fc70608498e24b1151ccfc9fd641
Nick Nelson <nicholasadamnelson@gmail.com>
2022-10-03 22:34:47 -0400

    Update Winget-Install-Apps.ps1

--- Winget-Install-Apps.ps1
+++ Winget-Install-Apps.ps1
@@ -1,38 +1,44 @@
 # Get list of all apps installed on current machine that areavilable to winget
 winget export -o c:\temp\installed.log
 # Convert that list to one like I have below
+
 # Gaming
 winget install -e --id Discord.Discord
 # winget install -e --id EpicGames.EpicGamesLauncher
 winget install -e --id Libretro.RetroArch
 winget install -e --id Nvidia.PhysX
 winget install -e --id Valve.Steam
+
 # Sytem
 winget install -e --id 7zip.7zip
-# winget install -e --id dnGrep.dnGrep
-# winget install -e --id JohnMacFarlane.Pandoc
+winget install -e --id dnGrep.dnGrep
+winget install -e --id JohnMacFarlane.Pandoc
 winget install -e --id Logitech.GHUB
 winget install -e --id Microsoft.Edge
 winget install -e --id Microsoft.EdgeWebView2Runtime
 winget install -e --id Microsoft.Office
 winget install -e --id Microsoft.OneDrive
-# winget install -e --id Microsoft.Teams
+winget install -e --id Microsoft.Teams
 winget install -e --id Mozilla.Firefox
-# winget install -e --id Nvidia.Broadcast
+winget install -e --id Nvidia.Broadcast
 winget install -e --id Obsidian.Obsidian
-# winget install -e --id Olivia.VIA
-# winget install -e --id Oracle.JavaRuntimeEnvironment
-winget install -e --id Plex.Plex
-# winget install -e --id qBittorrent.qBittorrent
-winget install -e --id Spotify.Spotify
-# winget install -e --id tailscale.tailscale
-winget install -e --id VideoLAN.VLC
+winget install -e --id Olivia.VIA
+winget install -e --id Oracle.JavaRuntimeEnvironment
+winget install -e --id tailscale.tailscale
 winget install -e --id voidtools.Everything
+winget install -e --id qBittorrent.qBittorrent
+
+# Media
+winget install -e --id Plex.Plex
+winget install -e --id Spotify.Spotify
+winget install -e --id VideoLAN.VLC
+
 # Coding
 winget install -e --id GitHub.GitHubDesktop
 winget install -e --id Git.Git
 winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id Microsoft.VisualStudioCode
+
 # Runtimes
 winget install -e --id Microsoft.DotNet.DesktopRuntime.3_1
 winget install -e --id Microsoft.VC++2005Redist-x86
@@ -47,6 +53,6 @@ winget install -e --id Microsoft.VC++2013Redist-x64
 winget install -e --id Microsoft.VC++2013Redist-x86
 winget install -e --id Microsoft.VC++2015-2022Redist-x64
 winget install -e --id Microsoft.VC++2015-2022Redist-x86
+
 # Upgrade all
 winget upgrade --all
-#
