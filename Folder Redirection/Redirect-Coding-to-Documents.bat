b2600e01a7c8fbc7963b2c014491055c4c1f5115
Nick Nelson <nick@chrxnn.com>
2022-10-17 00:01:29 -0400

    changes

--- Folder Redirection/Redirect-Coding-to-Documents.bat
+++ Folder Redirection/Redirect-Coding-to-Documents.bat
@@ -1,4 +1,3 @@
 mklink /j %UserProfile%\.ssh "%UserProfile%\Documents\Appdata\.ssh"
 mklink /j %UserProfile%\.android "%UserProfile%\Documents\Appdata\.android"
-mklink /j %UserProfile%\.vscode "%UserProfile%\Documents\Appdata\.vscode"
 mklink /H %UserProfile%\.gitconfig "%UserProfile%\Documents\Appdata\.gitconfig"
