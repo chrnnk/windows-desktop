a32234cf9fb3172576e11625b6b92046741d98c2
Nick Nelson <nick@chrxnn.com>
2022-10-31 12:02:51 -0400

    Update ps-setup.ps1

--- Powershell/ps-setup.ps1
+++ Powershell/ps-setup.ps1
@@ -5,6 +5,21 @@ Write-Host "Run this as admin or it will start failing pretty quick"
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Set-ExecutionPolicy RemoteSigned 
 
+Write-Host "OMP Install"
+winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
+
+Write-Host "Font Download, unzip and install these fonts, then set as Default profile Appearance in Windows Terminal"
+Invoke-RestMethod https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip?WT.mc_id=-blog-scottha -o cove.zip
+
+Write-Host "Winfetch Install"
+Install-Script -Name pwshfetch-test-1
+
+Write-Host "PSReadLine Install"
+Install-Module PSReadLine -Force
+
+Write-Host "Install Terminal-Icons"
+Install-Module Terminal-Icons
+
 #If the file does not exist, create it.
 if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
      try {
@@ -23,18 +38,3 @@ if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
 		 Write-Host "[$PROFILE] already exists, not touched."
  }
 & $profile
-
-Write-Host "OMP Install"
-winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
-
-Write-Host "Font Download, unzip and install these fonts, then set as Default profile Appearance in Windows Terminal"
-Invoke-RestMethod https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip?WT.mc_id=-blog-scottha -o cove.zip
-
-Write-Host "Winfetch Install"
-Install-Script -Name pwshfetch-test-1
-
-Write-Host "PSReadLine Install"
-Install-Module PSReadLine -Force
-
-Write-Host "Install Terminal-Icons"
-Install-Module Terminal-Icons
