4b23e2a6f8e52ce91b8856a1930506d83b1c314d
Nick Nelson <nick@chrxnn.com>
2022-10-31 12:01:49 -0400

    Update ps-setup.ps1

--- Powershell/ps-setup.ps1
+++ Powershell/ps-setup.ps1
@@ -20,27 +20,17 @@ if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
  }
 & $profile
 
-Write-Host " "
-Write-Host "`nOMP Install`n"
-Write-Host " "
+Write-Host "OMP Install"
 winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
 
-Write-Host " "
 Write-Host "Font Download, unzip and install these fonts, then set as Default profile Appearance in Windows Terminal"
-Write-Host " "
 Invoke-RestMethod https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CascadiaCode.zip?WT.mc_id=-blog-scottha -o cove.zip
 
-Write-Host " "
 Write-Host "Winfetch Install"
-Write-Host " "
 Install-Script -Name pwshfetch-test-1
 
-Write-Host " "
 Write-Host "PSReadLine Install"
-Write-Host " "
-Install-Module PSReadLine
+Install-Module PSReadLine -Force
 
-Write-Host " "
 Write-Host "Install Terminal-Icons"
-Write-Host " "
 Install-Module Terminal-Icons
