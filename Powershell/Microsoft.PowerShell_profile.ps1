9514789e4095475f52730ffb2351c482d67c70e6
Nick Nelson <nick@chrxnn.com>
2022-10-31 01:27:15 -0400

    Create Microsoft.PowerShell_profile.ps1

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+Import-Module Terminal-Icons
+Import-Module PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+Set-Alias winfetch pwshfetch-test-1
+winfetch
