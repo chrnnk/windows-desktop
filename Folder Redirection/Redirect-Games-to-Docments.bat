c5137c47afebc39237b22e4e0e5f57bace8da8b7
Nick Nelson <nick@chrxnn.com>
2022-11-02 20:48:51 -0400

    nier

--- Folder Redirection/Redirect-Games-to-Docments.bat
+++ Folder Redirection/Redirect-Games-to-Docments.bat
@@ -9,3 +9,5 @@ mklink /j "C:\Games\World of Warcraft\_retail_\WTF\SavedVariables" "%UserProfile
 mklink /j "C:\Games\World of Warcraft\_classic_\Interface" "%UserProfile%\Documents\Saved Games\World of Warcraft\_classic_\Interface"
 mklink /j "C:\Games\World of Warcraft\_classic_\WTF\Account" "%UserProfile%\Documents\Saved Games\World of Warcraft\_classic_\WTF\Account"
 mklink /j "C:\Games\World of Warcraft\_classic_\WTF\SavedVariables" "%UserProfile%\Documents\Saved Games\World of Warcraft\_classic_\WTF\SavedVariables"
+
+mklink /j "%UserProfile%\Documents\My Games\NieR_Automata" "%UserProfile%\Documents\Saved Games\NieR_Automata"
