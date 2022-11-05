e38ea04bee64f3b2c47a9702c8c4921ef4419300
Nick Nelson <nick@chrxnn.com>
2022-11-05 16:07:26 -0400

    renames

--- Folder Redirection/Redirect-Games-to-Documents.bat
+++ Folder Redirection/Redirect-Games-to-Documents.bat
@@ -1,7 +1,3 @@
-# Creates fake link in first location to actual link in second location
-# Delete the existing folders in "C:\Games\World of Warcraft\_retail_\" before you point them to new
-# OR move out the existing WTF and Interface folders to your "Documents\Saved Games" before running this command
-
 mklink /j "C:\Games\World of Warcraft\_retail_\Interface" "%UserProfile%\Documents\Saved Games\World of Warcraft\_retail_\Interface"
 mklink /j "C:\Games\World of Warcraft\_retail_\WTF\Account" "%UserProfile%\Documents\Saved Games\World of Warcraft\_retail_\WTF\Account"
 mklink /j "C:\Games\World of Warcraft\_retail_\WTF\SavedVariables" "%UserProfile%\Documents\Saved Games\World of Warcraft\_retail_\WTF\SavedVariables"
---
+++ Folder Redirection/Redirect-Terminal-to-Documents.bat
@@ -0,0 +1,1 @@
+mklink /j "%LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" "%UserProfile%\Documents\Appdata\.terminal"
