97402d1251949715f40363ce041ba10e25e0df98
Nick Nelson <nick@chrxnn.com>
2022-11-18 18:34:17 -0500

    Update Redirect-Documents-to-OneDrive.bat

--- Folder Redirection/Redirect-Documents-to-OneDrive.bat
+++ Folder Redirection/Redirect-Documents-to-OneDrive.bat
@@ -1,7 +1,17 @@
 mklink /j "%UserProfile%\Documents" "%UserProfile%\OneDrive - domain.com\Documents"
 
-MOVE /-Y "%UserProfile%\Downloads\*.*" "%UserProfile%\OneDrive - chrxnn.com\Documents\Downloads"
-DEL "%UserProfile%\Downloads"
+mklink /j "%UserProfile%\Desktop" "%UserProfile%\OneDrive - chrxnn.com\Documents\Desktop"
+
 mklink /j "%UserProfile%\Downloads" "%UserProfile%\OneDrive - chrxnn.com\Documents\Downloads"
 
+mklink /j "%UserProfile%\Music" "%UserProfile%\OneDrive - chrxnn.com\Documents\Music"
+
+mklink /j "%UserProfile%\Pictures" "%UserProfile%\OneDrive - chrxnn.com\Documents\Pictures"
+
+mklink /j "%UserProfile%\Saved Games" "%UserProfile%\OneDrive - chrxnn.com\Documents\Saved Games"
+
 pause
+
+# MOVE /-Y "%UserProfile%\Saved Games\*.*" "%UserProfile%\OneDrive - chrxnn.com\Documents\Saved Games"
+# REN "%UserProfile%\Saved Games" "OLD-Saved Games"
+# mklink /j "%UserProfile%\Saved Games" "%UserProfile%\OneDrive - chrxnn.com\Documents\Saved Games"
