bf89b50838281cd7edd9f2facbe1c14f16639204
Nick Nelson <nick@chrxnn.com>
2023-02-20 22:24:22 -0500

    update winfetch install method

--- Customization/PowerShell/ps-setup.ps1
+++ Customization/PowerShell/ps-setup.ps1
@@ -59,4 +59,4 @@ if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
 		 Write-Host "`r`n[$PROFILE] already exists, not touched.`r`n"
  }
 
-Write-Host "`r`nBrowse to $DownloadsFolder\cove, select and install all fonts. Then set ""CaskaydiaCove Nerd Font Mono"" as Default profile Appearance in Windows Terminal`r`n"
+Write-Host "`r`nBrowse to Downloads\cove, select and install all fonts. Then set ""CaskaydiaCove Nerd Font Mono"" as Default profile Appearance in Windows Terminal`r`n"
