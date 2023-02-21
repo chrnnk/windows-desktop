b202d4b3a53cb4f8695af92e69d4d7709ac361f0
Nick Nelson <nick@chrxnn.com>
2023-02-20 21:20:35 -0500

    Update Redirect-Terminal-to-Documents.bat

--- Folder Redirection/Redirect-Terminal-to-Documents.bat
+++ Folder Redirection/Redirect-Terminal-to-Documents.bat
@@ -1,1 +1,1 @@
-mklink /j "%LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" "%UserProfile%\Documents\Appdata\.terminal"
+mklink /j "%LocalAppData%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" "%OneDrive%\Documents\Appdata\.terminal"
