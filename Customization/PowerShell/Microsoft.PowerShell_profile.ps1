6bdb4449555d017d9be0dd9167e009c022dac6a1
Nick Nelson <nick@chrxnn.com>
2023-02-20 22:48:08 -0500

    updating winfetch, cleaning up output

--- Customization/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Customization/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,11 @@
+Write-Host Initializing Oh-my-posh
 oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
-
 Import-Module Terminal-Icons
-
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
-
-function wgup([string]$var1, [string]$var2)
+function wgup()
 {
     winget upgrade --all
 }
-
-Set-Alias winfetch pwshfetch-test-1
-
 winfetch
