91090278efe21d9eda2e4005fba03b00eea70dd3
Nick Nelson <nick@chrxnn.com>
2023-02-20 21:19:14 -0500

    Update Redirect-Documents-to-OneDrive.bat

--- Folder Redirection/Redirect-Documents-to-OneDrive.bat
+++ Folder Redirection/Redirect-Documents-to-OneDrive.bat
@@ -1,17 +1,12 @@
-mklink /j "%UserProfile%\Documents" "%UserProfile%\OneDrive - domain.com\Documents"
-
-mklink /j "%UserProfile%\Desktop" "%UserProfile%\Documents\Desktop"
-
-mklink /j "%UserProfile%\Downloads" "%UserProfile%\Documents\Downloads"
-
-mklink /j "%UserProfile%\Music" "%UserProfile%\Documents\Music"
-
-mklink /j "%UserProfile%\Pictures" "%UserProfile%\Documents\Pictures"
-
-mklink /j "%UserProfile%\Saved Games" "%UserProfile%\Documents\Saved Games"
+ECHO mklink /j "%UserProfile%\Documents" "%UserProfile%\OneDrive - domain.com\Documents"
+ECHO mklink /j "%UserProfile%\Desktop" "%UserProfile%\Documents\Desktop"
+ECHO mklink /j "%UserProfile%\Downloads" "%UserProfile%\Documents\Downloads"
+ECHO mklink /j "%UserProfile%\Music" "%UserProfile%\Documents\Music"
+ECHO mklink /j "%UserProfile%\Pictures" "%UserProfile%\Documents\Pictures"
+ECHO mklink /j "%UserProfile%\Saved Games" "%UserProfile%\Documents\Saved Games"
 
 pause
 
 # MOVE /-Y "%UserProfile%\Saved Games\*.*" "%UserProfile%\Documents\Saved Games"
 # REN "%UserProfile%\Saved Games" "OLD-Saved Games"
-# mklink /j "%UserProfile%\Saved Games" "%UserProfile%\Documents\Saved Games"
+# mklink /j "%UserProfile%\Saved Games" "%OneDrive%\Documents\Saved Games"
