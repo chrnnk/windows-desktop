5ec07140141012d1b54cf400c2430db583e171e1
Nick Nelson <nick@chrxnn.com>
2023-03-12 14:22:45 -0400

    change adapter to "ethernet"

--- Scripts/DNS-Toggle.bat
+++ Scripts/DNS-Toggle.bat
@@ -3,10 +3,10 @@
 :: replace "Ethernet" below with the name of your adapter
 
 echo set DNS to Cloudflare
-netsh interface ipv4 set dns "Ethernet 3" static 1.1.1.2
-netsh interface ipv4 add dns "Ethernet 3" 1.0.0.2 index=2
+netsh interface ipv4 set dns "Ethernet" static 1.1.1.2
+netsh interface ipv4 add dns "Ethernet" 1.0.0.2 index=2
 pause
 
 echo set DNS back to DHCP
-netsh interface ipv4 set dns "Ethernet 3" dhcp
+netsh interface ipv4 set dns "Ethernet" dhcp
 TIMEOUT /T 5
