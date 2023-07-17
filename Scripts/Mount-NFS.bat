2ae69c4e91bb6d5ec43dd21d195d3215c710e1c4
Nick Nelson <nick@chrxnn.com>
2023-07-17 17:24:35 -0400

    Update Mount-NFS.bat

--- Scripts/Mount-NFS.bat
+++ Scripts/Mount-NFS.bat
@@ -1,2 +1,2 @@
-mount -o anon \\unraid\mnt\user\media Z:
-mount -o anon \\unraid\mnt\user\data Y:
+mount -o anon \\xerxes.home.chrnnk.tv\mnt\user\media Z:
+mount -o anon \\xerxes.home.chrnnk.tv\mnt\user\data Y:
