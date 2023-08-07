c14df04393180646a2959bed790f54a329769d79
Nick Nelson <nick@chrxnn.com>
2023-08-06 21:46:53 -0400

    Update App-Export.ps1

--- Scripts/App-Export.ps1
+++ Scripts/App-Export.ps1
@@ -1,4 +1,4 @@
-# Get list of all apps installed on current machine that are avilable to winget
+# Export list of all apps installed on current machine that are avilable to winget
 $DownloadsFolder = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
 $Date = (Get-Date -Format "yyMMdd")
 winget export -o $DownloadsFolder\installed$Date.log
