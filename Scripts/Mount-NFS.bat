62cece8386161b3e0de424557723e99b598b3b6b
Nick Nelson <nick@chrxnn.com>
2023-09-27 11:39:59 -0400

    Update Mount-NFS.bat
    changed dns name, updated drive letters

--- Scripts/Mount-NFS.bat
+++ Scripts/Mount-NFS.bat
@@ -1,3 +1,3 @@
-mount -o anon \\xerxes.home.chrnnk.tv\mnt\user\media Z:
-mount -o anon \\xerxes.home.chrnnk.tv\mnt\user\data Y:
-mount -o anon \\xerxes.home.chrnnk.tv\mnt\user\tdarr X:
+mount -o anon \\xerxes-unraid.home.chrnnk.tv\mnt\user\media Y:
+mount -o anon \\xerxes-unraid.home.chrnnk.tv\mnt\user\data X:
+mount -o anon \\xerxes-unraid.home.chrnnk.tv\mnt\user\tdarr W:
