d9276bc18f3451be4201d08872d153a37220cc10
Nick Nelson <nick@chrnnk.tv>
2023-11-09 20:50:15 -0500

    Update ps-setup.ps1

--- PowerShell/ps-setup.ps1
+++ PowerShell/ps-setup.ps1
@@ -3,8 +3,8 @@
 Write-Host "`r`nRun this as admin or it will start failing pretty quick`r`n"
 Pause
 
-#Write-Host "`r`nInstalling Microsoft.VCRedist.2015+.x64`r`n"
-#winget install -e --id Microsoft.VCRedist.2015+.x64
+# Write-Host "`r`nInstalling Microsoft.VCRedist.2015+.x64`r`n"
+# winget install -e --id Microsoft.VCRedist.2015+.x64
 
 Write-Host "`r`nInstalling Microsoft.WindowsTerminal`r`n"
 winget install -e --id Microsoft.WindowsTerminal
@@ -33,8 +33,8 @@ Expand-Archive -Path $DownloadsFolder/cove.zip -DestinationPath $DownloadsFolder
 Write-Host "`r`nInstalling Neofetch-Win`r`n"
 winget install neofetch-win
 
-#Write-Host "`r`nInstalling PSReadLine`r`n"
-#Install-Module PSReadLine -Force
+# Write-Host "`r`nInstalling PSReadLine`r`n"
+# Install-Module PSReadLine -Force
 
 Write-Host "`r`nInstalling Terminal-Icons`r`n"
 Install-Module Terminal-Icons
@@ -42,7 +42,7 @@ Install-Module Terminal-Icons
 Write-Host "`r`nSince we're here, winget upgrade everything else`r`n"
 winget upgrade --all
 
-#If the file does not exist, create it.
+# If the file does not exist, create it.
 if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
      try {
          Invoke-RestMethod https://raw.githubusercontent.com/chrxnn/windows-desktop/main/Powershell/Microsoft.PowerShell_profile.ps1 -o $PROFILE
